72b48a32061b34c4a719d4e1f1a158bb9ac03cf1
royalrishitha <145558208+royalrishitha@users.noreply.github.com>
2023-09-20 16:03:17 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,55 @@
+//  /* Iteration 4: Make Everything Work */
+//   Wonder Woman - background, hair, body, armour, costume, star, eyes
+var background = document.getElementById ("background");
+var buttonbackground = document.getElementById("buttonbackground");
+
+buttonbackground.oninput = () => {
+       //   console.log("my color input is working");
+        background.style.fill = buttonbackground.value;
+}; 
+var hair = document.getElementById ("hair");
+var buttonhair = document.getElementById("buttonhair");
+
+buttonhair.oninput = () => {
+       //   console.log("my color input is working");
+        hair.style.fill = buttonhair.value;
+};
+var body = document.getElementById ("body");
+var buttonbody = document.getElementById("buttonbody");
+
+buttonbody.oninput = () => {
+       //   console.log("my color input is working");
+        body.style.fill = buttonbody.value;
+};
+var armour = document.getElementById ("armour");
+var buttonarmour = document.getElementById("buttonarmour");
+
+buttonarmour.oninput = () => {
+       //   console.log("my color input is working");
+        armour.style.fill = buttonarmour.value;
+};
+var costume = document.getElementById ("costume");
+var buttoncostume = document.getElementById("buttoncostume");
+
+buttoncostume.oninput = () => {
+       //   console.log("my color input is working");
+        costume.style.fill = buttoncostume.value;
+};
+var star = document.getElementById ("star");
+var buttonstar = document.getElementById("buttonstar");
+
+buttonstar.oninput = () => {
+       //   console.log("my color input is working");
+        star.style.fill = buttonstar.value;
+};
+var eyes = document.getElementById ("eyes");
+var buttoneyes = document.getElementById("buttoneyes");
+
+buttoneyes.oninput = () => {
+       //   console.log("my color input is working");
+        eyes.style.fill = buttoneyes.value;
+};
+
+
+
+
